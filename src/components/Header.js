7b5a536f24e912384b583edab7469b7f60621565
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-13 18:37:02 -0300

    Header theme icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 const Header = () => {
   return (
@@ -14,15 +15,23 @@ const Header = () => {
         <div className="searchIconContainer">
           <img src={searchIcon} />
         </div>
-        <input className="searchInput" placeholder="Collection, item or user..."/>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user..."
+        />
       </div>
 
-        <div className="headerItems">
+      <div className="headerItems">
         <p>Drops</p>
         <p>Marketplace</p>
         <p>Create</p>
-        </div>
+      </div>
 
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+            <img src={themeSwitchIcon}></img>
+        </div>
+      </div>
     </div>
   );
 };
