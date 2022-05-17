d3b02073e0250371fd5531529753d99aec65ad71
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-17 15:16:13 -0300

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import "./App.css";
-import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Punklist from "./components/Punklist";
 import Main from "./components/Main";
 
+
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+  
+  
 
-  useEffect(() => {
+  useEffect(() => {  
     const getMyfNfts = async () => {
       const openseaData = await axios.get(
         "https://testnets-api.opensea.io/assets?asset_contract_address=0xC77612e038dB505203b282574639bDB72a028482&order_direction=asc"
       );
-      console.log(openseaData.data.assets);
+      // console.log(openseaData.data.assets);
       setPunkListData(openseaData.data.assets);
     };
-    getMyfNfts();
+    getMyfNfts()
   }, []);
+
 
   return (
     <div className="app">
       <Header />
-      {/* <CollectionCard id={0} 
-      name={'Bandana Punk'} 
-      traits={[{'value': 7}]} 
-      image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600"
-      /> */}
-      <Main/>
-      <Punklist punkListData={punkListData} />
+      {
+        punkListData.length > 0 && (
+          <>
+            <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+            <Punklist setSelectedPunk={setSelectedPunk} punkListData={punkListData}  />
+          </>
+        )
+      }
+      
+      
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import instagramLogo from "../assets/owner/instagram.png";
 import twitterLogo from "../assets/owner/twitter.png";
 import moreIcon from "../assets/owner/more.png";
 import "./Main.css";
 
-const Main = () => {
+const Main = ({ selectedPunk, punkListData }) => {
+  const [activePunk, setActivePunk] = useState(punkListData[0]);
+
+  useEffect(() => {
+    setActivePunk(punkListData[selectedPunk]);
+  }, [punkListData, selectedPunk]);
+
   return (
     <div className="main">
       <div className="mainContent">
         <div className="punkHighlight">
           <div className="punkContainer">
-            <img className="selectedPunk" src={'https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600'} alt="" />
+            <img
+              className="selectedPunk"
+              src={activePunk.image_original_url}
+              alt=""
+            />
           </div>
         </div>
+
         <div className="punkDetails" style={{ color: "#fff" }}>
-          <div className="title">{"Bandana Punk"}</div>
-          <span className="itemNumber">.#3</span>
-        </div>
-        <div className="owner">
-          <div className="ownerImageContainer">
-            <img src={'https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600'} alt="" />
+          <div className="title">
+            {activePunk.name}
+            <span className="itemNumber">·#{activePunk.token_id}</span>
           </div>
-          <div className="ownerDetails">
-            <div className="ownerNameAndHandle">
-              <div>asdasdasdasdasdasd</div>
-              <div className="ownerHandle">@LonelyPines</div>
+
+          <div className="owner">
+            <div className="ownerImageContainer">
+              <img src={activePunk.owner.profile_img_url} alt="" />
             </div>
-            <div className="ownerLink">
-              <img src={instagramLogo} alt="" />
+
+            <div className="ownerDetails">
+              <div className="ownerNameAndHandle">
+                <div>{activePunk.owner.address}</div>
+                <div className="ownerHandle">@patricioMonpelat</div>
+              </div>
+              <div className="ownerLink">
+                <img src={instagramLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={twitterLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={moreIcon} alt="" />
+              </div>
             </div>
-            <div className="ownerLink">
-              <img src={twitterLogo} alt="" />
-            </div>
-            <div className="ownerLink">
-              <img src={moreIcon} alt="" />
-            </div>
+
           </div>
+
         </div>
+
       </div>
     </div>
   );
